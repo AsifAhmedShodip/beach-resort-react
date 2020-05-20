6f12780c1b8aa037a096a8655b9930e806e22a2a
AsifAhmedShodip <asifahmedshodip@gmail.com>
2020-05-20 08:42:35 +0600

    dfghb

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ import {
 function App() {
   return (
     <>
-      <Router>
+      <Router basename={process.env.PUBLIC_URL}>
         <NavigationBar> </NavigationBar>
         <Switch>
           <Route path = '/' exact component = {Home}/>
